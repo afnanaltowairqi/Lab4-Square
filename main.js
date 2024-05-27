0c41d7b70cfc5d52ae057dc24aea120a0ad35891
iiafn <afnan_twirqi@hotmail.com>
2024-05-27 21:12:25 +0300

    add Lab4

---
+++ main.js
@@ -0,0 +1,7 @@
+let width = document.getElementById("width");
+let height = document.getElementById("height");
+let color = document.getElementById("color");
+let radius = document.getElementById("radius");
+let set = document.getElementById("set");
+let reset = document.getElementById("reset");
+
