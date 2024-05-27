61baaa4443781a15af45c735baa6c5cd643fb866
iiafn <afnan_twirqi@hotmail.com>
2024-05-27 21:34:53 +0300

    add Lab4

--- main.js
+++ main.js
@@ -1,7 +1,15 @@
-let width = document.getElementById("width");
-let height = document.getElementById("height");
-let color = document.getElementById("color");
-let radius = document.getElementById("radius");
-let set = document.getElementById("set");
-let reset = document.getElementById("reset");
+let input = document.querySelectorAll("input");
+let cube = document.getElementById("cube1");
 
+function colorC(e) {
+    cube.style.backgroundColor = e
+}
+function widthC(e) {
+    cube.style.width = e +"vw";
+}
+function heightC(e) {
+    cube.style.height = e +"vh";
+}
+function borderR(e) {
+    cube.style.borderRadius = e + "px";
+}
